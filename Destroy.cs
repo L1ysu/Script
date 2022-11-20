2f186d11056d07230a10a97c9e67ed08d4c1edb6
Liysu <118666285+L1ysu@users.noreply.github.com>
2022-11-20 15:17:43 +0300

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject); //я покушала и подключила команду, программу для уничтожения игрового объекта в начале игры!!!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
